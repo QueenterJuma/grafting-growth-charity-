90cdf37d6b8a95d5df55afaca84c9dbba81a43ca
QueenterJuma <jumahqueenter@gmail.com>
2025-03-20 22:46:28 +0300

    Add Gallery

---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Gallery.css';
+
+const Gallery = () => {
+  const images = [
+    '/images/gallery1.jpg',
+    '/images/gallery2.jpg',
+    '/images/gallery3.jpg',
+    '/images/gallery4.jpg'
+  ];
+
+  return (
+    <section className="gallery animate-zoom">
+      <h2 className="section-title">Gallery</h2>
+      <div className="gallery-grid">
+        {images.map((src, idx) => (
+          <img key={idx} src={src} alt={`Gallery ${idx + 1}`} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
