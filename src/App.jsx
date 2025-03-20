ca5d53d05ca4fa53db82497845117ec30752c883
QueenterJuma <jumahqueenter@gmail.com>
2025-03-20 17:35:28 +0300

    Create React Vite project

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+// import Footer from './components/Footer'
+// import News from './pages/News';
+// import Resources from './components/Resources/Resources'
+// import Banner from './components/Banner'
+// import Homepage from './components/Homepage'
+
+function App() {
+
+  return (
+    <div >
+      <BrowserRouter>
+  <Navbar />
+        <Routes>
+          <Route path='/' element={ <Home/> } />
+          {/* <Route path='/news' element={ <News /> } /> */}
+          {/* <Route path='/resources' element={ <Resources /> } /> */}
+        {/*  <Route path='/about' element={ <About /> } />
+          <Route path='/contact' element={ <Contact /> } /> */}
+        </Routes>        
+
+        {/* <Footer /> */}
+      </BrowserRouter>
+      
+    </div>
+  )
+}
+
+export default App
